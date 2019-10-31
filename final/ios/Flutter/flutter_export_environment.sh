277895689448b1fabc51858de78fbc7465d43456
jaehoonx2 <21300698@handong.edu>
2019-11-01 01:03:24 +0900

    final layout

---
+++ final/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jaehoon/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jaehoon/AndroidStudioProjects/flutter_classworks/final"
+export "FLUTTER_TARGET=/Users/jaehoon/AndroidStudioProjects/flutter_classworks/final/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jaehoon/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
